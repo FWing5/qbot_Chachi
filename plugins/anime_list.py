00009d2882b2a74ef1950dbcb498c9392e669af4
FWing5 <fwing5@outlook.com>
2024-10-12 17:41:01 +0800

    create and link the repo

---
+++ plugins/anime_list.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+def get_anime_list():
+
+    url = "https://api.bgm.tv/calendar"
+    
+    response = requests.get(url)
+
+    data = response.json()
+    with open('testdata.json', 'w') as file:
+        json.dump(data,file)
+
+get_anime_list()
+
+# def format_weather(city_name):
+
+#     city_encoded = city_name  # 重庆的URL编码
+#     weather_data = get_weather(city_encoded)
+
+#     # 检查是否返回了错误
+#     if 'error' in weather_data:
+#         return weather_data['error']
+#     else:
+#         # 实时天气
+#         realtime_weather = weather_data['realtime']
+#         result = f"实时天气:" + "\n" +  f"{realtime_weather['info']}, 温度: {realtime_weather['temperature']}℃, 湿度: {realtime_weather['humidity']}%, 风向: {realtime_weather['direct']}, 风力: {realtime_weather['power']}级, AQI: {realtime_weather['aqi']}"
+#         # 未来几天的天气
+#         result = result + "\n" + "未来几天的天气:"
+#         for day in weather_data['future']:
+#             result = result + "\n" + f"日期: {day['date']}, 天气: {day['weather']}, 温度: {day['temperature']}, 风向: {day['direct']}"
+#         return result
---
+++ test.py
@@ -0,0 +1,44 @@
+import asyncio
+import os
+
+import botpy
+from botpy import logging
+from botpy.ext.cog_yaml import read
+from botpy.message import GroupMessage, Message
+
+test_config = read(os.path.join(os.path.dirname(__file__),"config.yaml"))
+
+_log = logging.get_logger()
+
+class MyClient(botpy.Client):
+    async def on_ready(self):
+        _log.info(f"robot 「{self.robot.name}」 on_ready!")
+
+    async def on_group_at_message_create(self, message: GroupMessage):
+        messageResult = await message._api.post_group_message(
+            group_openid=message.group_openid,
+              msg_type=0, 
+              msg_id=message.id,
+              content= f"「{self.robot.name}」收到你@的消息了哦: 「{message.content}」")
+        _log.info(messageResult)
+
+# class MyClient(botpy.Client):
+#     async def on_ready(self):
+#         _log.info(f"robot '{self.robot.name}' on_ready!")
+
+#     async def on_at_message_create(self, message: Message):
+#         _log.info(message.author.avatar)
+#         if "sleep" in message.content:
+#             await asyncio.sleep(10)
+#         _log.info(message.author.username)
+#         await message.reply(content = f"{self.robot.name}收到你@的消息了哦:{message.content}")
+
+if __name__ == "__main__":
+    # 通过预设置的类型，设置需要监听的事件通道
+    # intents = botpy.Intents.none()
+    # intents.public_guild_messages=True
+
+    # 通过kwargs，设置需要监听的事件通道
+    intents = botpy.Intents(public_messages=True)
+    client = MyClient(intents=intents)
+    client.run(appid=test_config["appid"], secret=test_config["secret"])
